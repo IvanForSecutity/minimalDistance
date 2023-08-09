4dfe11cfb39cc84d75b65e849cc7e32a2d6d38f9
ivan_kravchuk_personal <v00900p@gmail.com>
2023-08-09 19:27:31 +0300

    some renaming && a test

--- src/main/java/MinimalDistance.java
+++ src/main/java/MinimalDistance.java
@@ -16,37 +16,37 @@ public class MinimalDistance {
     }
     
     public static void minimalDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n + 1][m + 1];
+        int firstWordLength = word1.length();
+        int secondWordLength = word2.length();
+        int[][] editDistanceMatrix = new int[firstWordLength + 1][secondWordLength + 1];
         
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = i;
+        for (int i = 0; i <= firstWordLength; i++) {
+            editDistanceMatrix[i][0] = i;
         }
-        for (int j = 0; j <= m; j++) {
-            dp[0][j] = j;
+        for (int j = 0; j <= secondWordLength; j++) {
+            editDistanceMatrix[0][j] = j;
         }
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                int deletion = dp[i][j - 1] + 1;
-                int insertion = dp[i - 1][j] + 1;
-                int substitution = dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1);
-                dp[i][j] = Math.min(Math.min(deletion, insertion), substitution);
+        for (int i = 1; i <= firstWordLength; i++) {
+            for (int j = 1; j <= secondWordLength; j++) {
+                int deletion = editDistanceMatrix[i][j - 1] + 1;
+                int insertion = editDistanceMatrix[i - 1][j] + 1;
+                int substitution = editDistanceMatrix[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1);
+                editDistanceMatrix[i][j] = Math.min(Math.min(deletion, insertion), substitution);
             }
         }
         
-        int distance = dp[n][m];
+        int distance = editDistanceMatrix[firstWordLength][secondWordLength];
         System.out.println(distance);
-        int curI = n;
-        int curJ = m;
+        int curI = firstWordLength;
+        int curJ = secondWordLength;
         String[] curWord = word2.split("");
         
         System.out.println(String.join("", curWord));
         while (distance > 0) {
-            int deletion = dp[curI][curJ - 1];
-            int insertion = dp[curI - 1][curJ];
-            int substitution = dp[curI - 1][curJ - 1];
+            int deletion = editDistanceMatrix[curI][curJ - 1];
+            int insertion = editDistanceMatrix[curI - 1][curJ];
+            int substitution = editDistanceMatrix[curI - 1][curJ - 1];
             if (substitution < distance) {
                 curWord[curJ - 1] = Character.toString(word1.charAt(curI - 1));
                 curI--;
--- src/test/java/MinimalDistanceTests.java
+++ src/test/java/MinimalDistanceTests.java
@@ -3,7 +3,8 @@ import org.junit.jupiter.api.Test;
 public class MinimalDistanceTests {
 
     @Test
-    public void testAdd() {
-        MinimalDistance.minimalDistance("args[0]", "args[1]");
+    public void testSomePair() {
+        MinimalDistance.minimalDistance("sitting", "kitten");
+        //in task description, w1 should be converted to w2, but in the minimalDistance, w2 is converted to w1
     }
 }
