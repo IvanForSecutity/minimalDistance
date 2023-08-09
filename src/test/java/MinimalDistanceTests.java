df1ac8b130ef9bf04ca7880fc9375e6e69500dc4
ivan_kravchuk_personal <v00900p@gmail.com>
2023-08-09 12:54:51 +0300

    setup maven project & test

---
+++ src/test/java/MinimalDistanceTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class MinimalDistanceTests {
+
+    @Test
+    public void testAdd() {
+        MinimalDistance.minimalDistance("args[0]", "args[1]");
+    }
+}
